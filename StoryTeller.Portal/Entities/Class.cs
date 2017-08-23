c706379ed333ecaf3090da58d141bf83d2e8b142
Jarrod Johnson <jarrod.johnson@fleetmatics.com>
2017-08-23 11:17:41 -0400

    Add solution with single ASP.net core website to use as shell

---
+++ StoryTeller.Portal/Entities/Class.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoryTeller.ResultAggregator.Entities
+{
+    public class Run
+    {
+        public int Id { get; set; }
+        public DateTime RunDateTime { get; set; }
+        public string Name { get; set; }
+        public string HtmlResult { get; set; }
+    }
+}
---
+++ StoryTeller.Portal/Entities/Spec.cs
@@ -0,0 +1,8 @@
+﻿namespace StoryTeller.ResultAggregator.Entities
+{
+    public class Spec
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
