5eb03fc1a345d439a9dde6fa560599afb55974a2
Jarrod Johnson <jarrod.johnson@fleetmatics.com>
2017-08-23 11:24:18 -0400

    Add basic CQRS plumbing

---
+++ StoryTeller.Portal.CQRS/Interfaces.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StoryTeller.Portal.CQRS
+{
+    public interface IQuery<TResult>
+    {
+    }
+
+    public interface IQueryHandler<in TQry, out TResult> where TQry : IQuery<TResult>
+    {
+        TResult Fetch(TQry qry);
+    }
+
+    public interface ICommand
+    {
+    }
+
+    public interface ICommandHandler<in TCmd> where TCmd : ICommand
+    {
+        void Execute(TCmd cmd);
+    }
+}
