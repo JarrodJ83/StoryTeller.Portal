419e44eb1b84112fe580fa315f92b7a00d623b1c
Jarrod Johnson <jarrod.johnson@fleetmatics.com>
2017-08-25 11:09:55 -0400

    Add StoryTeller.Portal.ResultsAggregator project for creating ST extension to send results back to portal

---
+++ StoryTeller.Portal.ResultsAggregator/IPortalResultsAggregatorClient.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using StoryTeller.ResultAggregation.ClientModel;
+using StoryTeller.ResultAggregation.Models;
+
+namespace StoryTeller.Portal.ResultsAggregator
+{
+    public interface IPortalResultsAggregatorClient
+    {
+        List<Spec> GetSpecs();
+        Spec AddSpec(PostSpec spec);
+        Run AddRun(PostRun run);
+        void AddSpecToRun(PostRunSpec runSpec);
+    }
+}
