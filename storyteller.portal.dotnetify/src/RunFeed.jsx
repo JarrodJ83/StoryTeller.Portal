d0b5366873d81f610ee3ac061d94fcc0e6342651
Jarrod Johnson <jarrod.johnson@fleetmatics.com>
2017-08-29 18:52:35 -0400

    Add result links to grid

--- storyteller.portal.dotnetify/src/RunFeed.jsx
+++ storyteller.portal.dotnetify/src/RunFeed.jsx
@@ -11,28 +11,32 @@ class RunFeed extends React.Component {
     render() {
         return (
             <div className="App-intro">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Id</th>
-                            <th>Run Date</th>
-                            <th>App</th>
-                            <th>Run Name</th>
-                            <th>Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.state.Runs.map(run => <tr key={run.Id}>
+            <table>
+            <thead>
+            <tr>
+                <th>Id</th>
+                <th>Run Date</th>
+                <th>App</th>
+                <th>Run Name</th>
+                <th>Status</th>
+                <th>Results</th>
+            </tr>
+            </thead>
+            <tbody>
+            {this.state.Runs.map(run => <tr key={run.Id}>
                                                         <td>{run.Id}</td>
                                                         <td>{run.RunDateTime}</td>
                                                         <td>{run.AppName}</td>
                                                         <td>{run.Name}</td>
                                                         <td>(Pass: {run.SuccessfulCount} Fail: {run.FailureCount} Total: {run.TotalCount})</td>
+                                                        <td>
+                                <a href={"viewResults?runId=" + run.Id} style={{visibility: run.Finished ? "visible" : "hidden"}} >View Results</a>
+                                                        </td>
                                                     </tr >)}
                     </tbody>
                 </table>                 
             </div>
         );
     }
-}
-export default RunFeed;
+            }
+            export default RunFeed;
