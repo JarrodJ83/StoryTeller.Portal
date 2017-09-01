dd30a0eb12c640542cd1f9ddc64e71ffbcc453bb
Jarrod Johnson <jarrod.johnson@fleetmatics.com>
2017-09-01 15:27:04 -0400

    Add color styling to RunFeed

--- storyteller.portal.dotnetify/src/RunFeed.jsx
+++ storyteller.portal.dotnetify/src/RunFeed.jsx
@@ -11,7 +11,8 @@ class RunFeed extends React.Component {
     getInitialState() {
         this.vm.onRouteEnter = (path, template) => template.Target = "HtmlResults";
     }
-    render() {
+    
+    render() {  
         return (
             <Table striped bordered condensed hover>
                 <thead>
@@ -31,7 +32,7 @@ class RunFeed extends React.Component {
                                                         <td>
                                 <a target="_blank" href={"http://localhost:1881/Runs/" + run.Id + "/results"} style={{visibility: run.Finished ? "visible" : "hidden"}} >View Results</a>
                                                         </td>
-                                                        <td><Glyphicon glyph={run.Finished ? run.Passed ? "ok" : "exclamation-sign" : "hourglass"} /></td>
+                                                        <td><Glyphicon glyph={run.Finished ? run.Passed ? "ok" : "exclamation-sign" : "hourglass"} style={{ color: run.Passed ? "green" : "red" }}/></td>
                                                     </tr >)}
                     </tbody>
                 </Table>  
