5b96ce468154c5f16239150aabb1db6cf52664b8
Jarrod Johnson <johnson.jarrod@gmail.com>
2018-03-10 10:45:18 -0500

    Add ability to send messages to the hub from API

---
+++ StoryTeller.Portal.Web/EventsBroadcaster.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
+using StoryTeller.Portal.Web.Hubs;
+using StoryTeller.ResultAggregation.Events;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StoryTeller.Portal.Web
+{
+    public class EventsBroadcaster : INotificationHandler<RunCreated>
+    {
+        public EventsBroadcaster(IHubContext<DashboardHub> dashboardHub)
+        {
+            DashboardHubContext = dashboardHub;
+        }
+
+        public IHubContext<DashboardHub>DashboardHubContext { get; }
+
+        public async Task Handle(RunCreated notification, CancellationToken cancellationToken)
+        {
+            await DashboardHubContext.Clients.All.SendAsync("Send", JsonConvert.SerializeObject(notification));
+        }
+    }
+}
--- StoryTeller.Portal.Web/Hubs/DashboardHub.cs
+++ StoryTeller.Portal.Web/Hubs/DashboardHub.cs
@@ -1,10 +1,20 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using MediatR;
+using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
+using StoryTeller.ResultAggregation.Events;
+using System.Diagnostics;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace StoryTeller.Portal.Web.Hubs
 {
     public class DashboardHub : Hub
     {
+        public DashboardHub()
+        {
+            Debug.WriteLine("hub ctor");
+
+        }
         public Task Send(string message)
         {
             return Clients.All.SendAsync("Send", message);
