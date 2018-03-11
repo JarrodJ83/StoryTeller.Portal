9d08fcc2f8ea8db2e8d5057a4b2a59e757fa1222
Jarrod Johnson <johnson.jarrod@gmail.com>
2018-03-11 19:17:36 -0400

    Add request and handler to pull back all run summaries for dashboard

---
+++ StoryTeller.Portal.RequestHandlers/RunSummaries.cs
@@ -0,0 +1,25 @@
+﻿using StoryTeller.Portal.CQRS;
+using StoryTeller.Portal.Models.Views;
+using StoryTeller.Portal.Queries;
+using Requests = StoryTeller.Portal.Requests;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StoryTeller.Portal.RequestHandlers
+{
+    public class RunSummaries : IRequestHandler<Requests.RunSummaries, List<RunSummary>>
+    {
+        public IQueryHandler<LatestRunSumarries, List<RunSummary>> LatestRunSummaries { get; }
+
+        public RunSummaries(IQueryHandler<LatestRunSumarries, List<RunSummary>> latestRunSummaries)
+        {
+            LatestRunSummaries = latestRunSummaries;
+        }
+
+        public async Task<List<RunSummary>> HandleAsync(Requests.RunSummaries request, CancellationToken cancellationToken)
+        {
+            return await LatestRunSummaries.FetchAsync(new LatestRunSumarries(), cancellationToken);
+        }
+    }
+}
