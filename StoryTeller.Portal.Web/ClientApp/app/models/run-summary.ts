b3d1ff98d5383836d893f27a7ab0ab51dec1a761
Jarrod Johnson <johnson.jarrod@gmail.com>
2018-03-11 14:57:32 -0400

    Create run summary client side model

---
+++ StoryTeller.Portal.Web/ClientApp/app/models/run-summary.ts
@@ -0,0 +1,12 @@
+﻿export class RunSummary {
+    public id: number;
+    public Name: string;
+    public AppId: number;
+    public AppName: string;
+    public RunDateTime: string;
+    public Passed: boolean;
+    public Finished: boolean;
+    public SuccessfulCount: number;
+    public FailureCount: number;
+    public TotalCount: number;
+}
