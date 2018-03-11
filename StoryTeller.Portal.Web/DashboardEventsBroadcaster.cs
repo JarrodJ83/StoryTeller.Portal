968ac3c2b58df9868bee1bf2c833e9888a69a1dd
Jarrod Johnson <johnson.jarrod@gmail.com>
2018-03-11 19:45:25 -0400

    Send updates to UI from API

--- StoryTeller.Portal.Web/DashboardEventsBroadcaster.cs
+++ StoryTeller.Portal.Web/DashboardEventsBroadcaster.cs
@@ -1,6 +1,8 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.SignalR;
 using Newtonsoft.Json;
+using StoryTeller.Portal.CQRS;
+using StoryTeller.Portal.Models.Views;
 using StoryTeller.Portal.Web.Hubs;
 using StoryTeller.ResultAggregation.Events;
 using System.Threading;
@@ -8,18 +10,21 @@ using System.Threading.Tasks;
 
 namespace StoryTeller.Portal.Web
 {
-    public class EventsBroadcaster : INotificationHandler<RunCreated>, INotificationHandler<RunCompleted>, INotificationHandler<RunSpecUpdated>
+    public class DashbaordEventsBroadcaster : INotificationHandler<RunCreated>, INotificationHandler<RunCompleted>, INotificationHandler<RunSpecUpdated>
     {
         public IHubContext<DashboardHub> DashboardHubContext { get; }
+        public IQueryHandler<Queries.SummaryOfRun, RunSummary> RunSummary { get; }
 
-        public EventsBroadcaster(IHubContext<DashboardHub> dashboardHub)
+        public DashbaordEventsBroadcaster(IHubContext<DashboardHub> dashboardHub, IQueryHandler<Queries.SummaryOfRun, RunSummary> runSummary)
         {
             DashboardHubContext = dashboardHub;
+            RunSummary = runSummary;
         }        
 
-        public async Task Handle(RunCreated notification, CancellationToken cancellationToken)
+        public async Task Handle(RunCreated runCreated, CancellationToken cancellationToken)
         {
-            await DashboardHubContext.Clients.All.SendAsync(nameof(RunCreated), notification);
+            RunSummary runSummary = await RunSummary.FetchAsync(new Queries.SummaryOfRun(runCreated.RunId), cancellationToken);
+            await DashboardHubContext.Clients.All.SendAsync(nameof(RunCreated), runSummary);
         }
 
         public async Task Handle(RunSpecUpdated notification, CancellationToken cancellationToken)
--- storyteller.portal.web/Startup.cs
+++ storyteller.portal.web/Startup.cs
@@ -104,7 +104,7 @@ namespace storyteller.portal.web
             services.AddTransient<IApiContext, ApiContext>();
             services.AddTransient<ApiAuthenticationMiddleware>();
 
-            services.AddTransient<INotificationHandler<RunCreated>, EventsBroadcaster>();
+            //services.AddTransient<INotificationHandler<RunCreated>, DashbaordEventsBroadcaster>();
             services.AddTransient<CQRS.ICommandHandler<AddRunForApplication>, AddRunForApplicationViaSql>();
             services.AddTransient<CQRS.ICommandHandler<AddRunResult>, AddRunResultViaSql>();
             services.AddTransient<CQRS.ICommandHandler<AddSpecToRun>, AddSpecToRunViaSql>();
@@ -133,13 +133,13 @@ namespace storyteller.portal.web
             services.AddTransient<CQRS.IRequestHandler<PutRunSpecRequest>, PutRunSpecRequestHandler>();
             services.AddTransient<CQRS.IRequestHandler<StoryTeller.Portal.Requests.RunSummaries, List<RunSummary>>, StoryTeller.Portal.RequestHandlers.RunSummaries>();
 
-            services.AddSingleton<MediatR.IMediator, MediatR.Mediator>();
+            //services.AddSingleton<MediatR.IMediator, MediatR.Mediator>();
             //services.AddTransient<MediatR.IMediator, MediatorExceptionHandlerDecorator>();
 
             services.AddMediatR(); 
             
-            services.AddSingleton(p => new MediatR.SingleInstanceFactory(p.GetService));
-            services.AddSingleton(p => new MediatR.MultiInstanceFactory(p.GetServices));            
+            //services.AddSingleton(p => new MediatR.SingleInstanceFactory(p.GetService));
+            //services.AddSingleton(p => new MediatR.MultiInstanceFactory(p.GetServices));            
         }
     }
 }
